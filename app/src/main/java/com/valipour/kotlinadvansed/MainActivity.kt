e5411b5fa74b21134d816b2947d5be3051156ef2
motahareh <motahrevalipour@gmail.com>
2023-11-04 10:20:45 +0330

    add lib SmoothBottomBar

--- app/src/main/java/com/valipour/kotlinadvansed/MainActivity.kt
+++ app/src/main/java/com/valipour/kotlinadvansed/MainActivity.kt
@@ -2,7 +2,9 @@ package com.valipour.kotlinadvansed
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.PopupMenu
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
 import com.valipour.kotlinadvansed.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
+    }
+
+    private fun setUpNavigationComponent(){
+        val navHostFragment:NavHostFragment= supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController=navHostFragment.navController
+        val popupMenu= PopupMenu(this,null)
+        popupMenu.inflate(R.menu.menu_bottom)
+        val menu=popupMenu.menu
+        binding.bottomBar.setupWithNavController(menu,navController)
+
     }
 }
---
+++ app/src/main/java/com/valipour/kotlinadvansed/ui/PlanFragment.kt
@@ -0,0 +1,17 @@
+package com.valipour.kotlinadvansed.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.valipour.kotlinadvansed.R
+class PlanFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_plan, container, false)
+    }
+}
