2d00d7d0d582b45c3459a89e8935b025c0ed5488
motahareh <motahrevalipour@gmail.com>
2023-11-04 15:23:15 +0330

    add NavigationView

--- app/src/main/java/com/valipour/kotlinadvansed/MainActivity.kt
+++ app/src/main/java/com/valipour/kotlinadvansed/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.widget.PopupMenu
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.valipour.kotlinadvansed.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,11 +14,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
+        setUpNavigationComponent();
     }
 
     private fun setUpNavigationComponent(){
         val navHostFragment:NavHostFragment= supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController=navHostFragment.navController
+        AppBarConfiguration.Builder(setOf(R.id.homeFragment,R.id.planFragment)).setOpenableLayout(
+            binding.drawerLayout).build()
+        binding.navView.setupWithNavController(navController)
         val popupMenu= PopupMenu(this,null)
         popupMenu.inflate(R.menu.menu_bottom)
         val menu=popupMenu.menu
